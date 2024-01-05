f65d918fad664fa59cde7d33b1b193c336501180
carlosttorres33 <kisamehatake33@comunidad.unam.mx>
2024-01-05 15:02:03 -0600

    Logout

--- app/src/main/java/com/carlostorres/comprayventa/fragmentos/FragmentCuenta.kt
+++ app/src/main/java/com/carlostorres/comprayventa/fragmentos/FragmentCuenta.kt
@@ -1,60 +1,50 @@
 package com.carlostorres.comprayventa.fragmentos
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.carlostorres.comprayventa.OpcionesLogin
 import com.carlostorres.comprayventa.R
+import com.carlostorres.comprayventa.databinding.FragmentCuentaBinding
+import com.google.firebase.auth.FirebaseAuth
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [FragmentCuenta.newInstance] factory method to
- * create an instance of this fragment.
- */
 class FragmentCuenta : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+    private lateinit var binding : FragmentCuentaBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+
+    private lateinit var mContext : Context
+
+    override fun onAttach(context: Context) {
+        mContext = context
+        super.onAttach(context)
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_cuenta, container, false)
+        binding  = FragmentCuentaBinding.inflate(layoutInflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment FragmentCuenta.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            FragmentCuenta().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        binding.btnCerrarSesion.setOnClickListener {
+            firebaseAuth.signOut()
+            startActivity(Intent(mContext, OpcionesLogin::class.java))
+            activity?.finishAffinity()
+        }
+
     }
+
+
+
 }
