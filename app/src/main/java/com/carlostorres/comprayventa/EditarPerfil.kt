969654c61c756b0f36edd05a971e23838431139b
DiasoCarlos <desarrollo02@diaso.com.mx>
2024-06-06 17:17:24 -0600

    Upload and Update UserImage in Firebase

--- app/src/main/java/com/carlostorres/comprayventa/EditarPerfil.kt
+++ app/src/main/java/com/carlostorres/comprayventa/EditarPerfil.kt
@@ -1,9 +1,18 @@
 package com.carlostorres.comprayventa
 
+import android.app.Activity
 import android.app.ProgressDialog
+import android.content.ContentValues
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.view.Menu
+import android.widget.PopupMenu
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import com.bumptech.glide.Glide
 import com.carlostorres.comprayventa.databinding.ActivityEditarPerfilBinding
 import com.google.firebase.auth.FirebaseAuth
@@ -11,12 +20,14 @@ import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
+import com.google.firebase.storage.FirebaseStorage
 
 class EditarPerfil : AppCompatActivity() {
 
     private lateinit var binding : ActivityEditarPerfilBinding
     private lateinit var firebaseAuth: FirebaseAuth
     private lateinit var progressDialog: ProgressDialog
+    private var imageUri : Uri? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -31,6 +42,10 @@ class EditarPerfil : AppCompatActivity() {
         progressDialog.setCanceledOnTouchOutside(false)
 
         cargarInfo()
+
+        binding.fabCambiarImg.setOnClickListener {
+            select_img_de()
+        }
 
     }
 
@@ -65,7 +80,7 @@ class EditarPerfil : AppCompatActivity() {
                         val codigo = codTelefono.replace("+","").toInt()
                         binding.codeSelector.setCountryForPhoneCode(codigo)
                     }catch (e: java.lang.Exception){
-                        Toast.makeText(this@EditarPerfil, e.message, Toast.LENGTH_SHORT).show()
+                        //Toast.makeText(this@EditarPerfil, e.message, Toast.LENGTH_SHORT).show()
                     }
 
                 }
@@ -77,4 +92,160 @@ class EditarPerfil : AppCompatActivity() {
             })
 
     }
+
+    private fun subirImagenStorage(){
+        progressDialog.setMessage("Subiendo imagen")
+        progressDialog.show()
+
+        val rutaImagen = "imagenesPerfil/" + firebaseAuth.uid
+        val ref = FirebaseStorage.getInstance().getReference(rutaImagen)
+        ref.putFile(imageUri!!)
+            .addOnSuccessListener { taskSnapshot ->
+                val uriTask = taskSnapshot.storage.downloadUrl
+                while (!uriTask.isSuccessful);
+                val urlImagenCargada = uriTask.result.toString()
+                if(uriTask.isSuccessful){
+                    actualizarImagenBD(urlImagenCargada)
+                }
+            }.addOnFailureListener {  e ->
+                progressDialog.dismiss()
+                Toast.makeText(this, "Error: ${e.message}", Toast.LENGTH_SHORT).show()
+            }
+    }
+
+    private fun actualizarImagenBD(urlImagenCargada: String) {
+        progressDialog.setMessage("Actualizando")
+        progressDialog.show()
+
+        val hashMap : HashMap<String, Any> = HashMap()
+
+        if (imageUri != null){
+            hashMap["urlImagenPerfil"] = urlImagenCargada
+        }
+
+        val ref = FirebaseDatabase.getInstance().getReference("Usuarios")
+        ref.child(firebaseAuth.uid!!)
+            .updateChildren(hashMap)
+            .addOnSuccessListener {
+                progressDialog.dismiss()
+                Toast.makeText(this, "Imagen Actualizada", Toast.LENGTH_SHORT).show()
+            }.addOnFailureListener { e ->
+                progressDialog.dismiss()
+                Toast.makeText(this, "Error: ${e.message}", Toast.LENGTH_SHORT).show()
+            }
+    }
+
+    private fun select_img_de() {
+        val popupMenu = PopupMenu(this, binding.fabCambiarImg)
+
+        popupMenu.menu.add(Menu.NONE, 1, 1, "Camara")
+        popupMenu.menu.add(Menu.NONE, 2, 2, "Galeria")
+
+        popupMenu.show()
+
+        popupMenu.setOnMenuItemClickListener { menuItem ->
+
+            val itemId = menuItem.itemId
+
+            if (itemId == 1) {
+                //camara
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                    concederPermisoCamara.launch(arrayOf(android.Manifest.permission.CAMERA))
+                } else {
+                    concederPermisoCamara.launch(
+                        arrayOf(
+                            android.Manifest.permission.CAMERA,
+                            android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+                        )
+                    )
+                }
+            } else if (itemId == 2) {
+                //Galeria
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU){
+                    imagenGaleria()
+                }else{
+                    concederPermisoAlmacenamiento.launch(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                }
+            }
+
+            return@setOnMenuItemClickListener true
+
+        }
+
+    }
+
+    private val concederPermisoCamara =
+        registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()
+        ) { result ->
+
+            var concedidoTodos = true
+            for (seConcede in result.values) {
+                concedidoTodos = concedidoTodos && seConcede
+            }
+
+            if (concedidoTodos) {
+                imagenCamara()
+            } else {
+                Toast.makeText(this, "Permiso Camara Denegado", Toast.LENGTH_SHORT).show()
+            }
+
+        }
+
+    private fun imagenCamara() {
+        val contentValues = ContentValues()
+        contentValues.put(MediaStore.Images.Media.TITLE, "Titulo_Imagen")
+        contentValues.put(MediaStore.Images.Media.DESCRIPTION, "Descripcion_Imagen")
+
+        imageUri = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues)
+
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri)
+
+        resultadoCamara_ARL.launch(intent)
+
+    }
+
+    private val resultadoCamara_ARL =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()){ result ->
+            if (result.resultCode == Activity.RESULT_OK){
+                subirImagenStorage()
+            }else{
+                Toast.makeText(this, "Cancelado", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+    private val concederPermisoAlmacenamiento = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { esConcedido ->
+
+        if (esConcedido) {
+            imagenGaleria()
+        } else {
+            Toast.makeText(this, "Permiso Galeria Denegado", Toast.LENGTH_SHORT).show()
+        }
+
+    }
+
+    private fun imagenGaleria() {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        resultadoGaleria_ARL.launch(intent)
+    }
+
+    private val resultadoGaleria_ARL =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()){ result->
+            if (result.resultCode == Activity.RESULT_OK){
+
+                val data = result.data
+                imageUri = data!!.data
+
+                subirImagenStorage()
+
+            }else{
+                Toast.makeText(this, "Cancelado", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+
 }
